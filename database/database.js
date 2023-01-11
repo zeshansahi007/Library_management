6d0528ce994517ab0fcc5607f8f747074bf35089
Zeeshan Ahmad <114911981+ZeeshanHaral@users.noreply.github.com>
2023-01-11 23:18:57 +0000

    Add files via upload

--- database/database.js
+++ database/database.js
@@ -1,8 +1,9 @@
 const mySql = require('mysql')
 const db = mySql.createConnection({
     host : 'localhost',
-    user : "root",
-    password : "",
+    port: 3306,
+    user : 'root',
+    password :"",
     database : 'library'
 })
 
